b416ba09eba4159b10d079002bdea36f2c0ad36c
SilviaGonz <151831773+SilviaGonz@users.noreply.github.com>
2025-01-12 00:31:30 +0000

    create class MainFrame

---
+++ src/main/java/software/ulpgc/MainFrame.java
@@ -0,0 +1,12 @@
+package software.ulpgc;
+
+import javax.swing.*;
+
+public class MainFrame implements JFrame {
+    public MainFrame() {
+        this.setTitle("Kata 7");
+        this.setSize(800, 600);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
