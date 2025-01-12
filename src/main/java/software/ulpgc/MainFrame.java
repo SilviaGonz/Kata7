449914c3f240f52d392c23e7b2e9e92d46b46923
SilviaGonz <151831773+SilviaGonz@users.noreply.github.com>
2025-01-12 00:36:53 +0000

    create the class MainFrame

---
+++ src/main/java/software/ulpgc/MainFrame.java
@@ -0,0 +1,13 @@
+package software.ulpgc;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+    public MainFrame() throws HeadlessException {
+        this.setTitle("Kata 7");
+        this.setSize(800, 600);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
